afcf64fe99e07ad1fea1f437e884f93b5b20e5f7
Teoh TeikToe <ttt@mensa.org.sg>
2024-03-18 14:44:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, render_template
 import google.generativeai as palm
+import replicate
+import os
 
 palm.configure(api_key="AIzaSyCCT1K99BJ1JbLwhCE7qOcQ5KOZcPJ9ZZ4")
 model = {
     "model": "models/chat-bison-001",
 }
+
+os.environ["REPLICATE_API_TOKEN"] = "787f515cb0624813736c11e7fefec66473394f02"
 
 app = Flask(__name__)
 
@@ -12,9 +16,16 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-
 @app.route("/palm", methods=["GET","POST"])
 def palm_flask():
+    return(render_template("palm.html"))
+
+@app.route("/mj", methods=["GET","POST"])
+def mj():
+    return(render_template("mj.html"))
+
+@app.route("/palm_query", methods=["GET","POST"])
+def palm_query():
     q = request.form.get("q")
     print(q)
     r = palm.chat(
@@ -22,7 +33,16 @@ def palm_flask():
         messages=q
     )
     print(r.last)
-    return(render_template("reply.html",r=r.last))
+    return(render_template("palm_reply.html",r=r.last))
+
+@app.route("/mj_query", methods=["GET","POST"])
+def mj_query():
+    q = request.form.get("q")
+    r = replicate.run(
+        "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf", 
+        input={"prompt": q}
+    )
+    return(render_template("mj_reply.html",r=r[0]))
 
 if __name__ == "__main__":
     app.run()
